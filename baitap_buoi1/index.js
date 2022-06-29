e1661929e535a604afc6b1a17ad6fed2b05808eb
trantoan11011996 <trantoan11011996@gmail.com>
2022-06-29 20:46:28 +0700

    fix baitap

--- baitap_buoi1/index.js
+++ baitap_buoi1/index.js
@@ -2,21 +2,26 @@ const Car =  function(make,speed){
     this.make = make;
     this.speed = speed;
 }
+
 Car.prototype.accelerate = function(){
-    console.log(this.speed += 10,"km/h")
+    this.speed += 10;
+    console.log(`this acclerate make speed go up to ${this.speed}km/h`)
 }
 
 Car.prototype.brake = function(){
-    console.log(this.speed -= 5,"km/h")
+    this.speed -= 5;
+    console.log(`this brake make speed down to ${this.speed}km/h`)
 }
+
 function TypeCar (type) {
     this.type = type
 }
+
 TypeCar.prototype = new Car()
 let car_BMW = new TypeCar("BMW")
 car_BMW.make = "titanium";
 car_BMW.speed = 100
-console.log(car_BMW)
+console.log(`${car_BMW.type} have a speed is ${car_BMW.speed}km/h`)
 car_BMW.accelerate()
 car_BMW.brake()
 
@@ -24,12 +29,8 @@ TypeCar.prototype = new Car()
 let car_KIA = new TypeCar("KIA")
 car_KIA.make = "titanium";
 car_KIA.speed = 90
-console.log(car_KIA)
+console.log(`${car_KIA.type} have a speed is ${car_KIA.speed}km/h`)
 car_KIA.accelerate()
 car_KIA.brake()
 const arr = []
 
-arr.push(car_KIA,car_BMW)
-localStorage.setItem('car',JSON.stringify(arr))
-const result = localStorage.getItem('car')
-console.log(JSON.parse(result))
